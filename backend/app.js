e6738ca41fc950aa31deaa0b60c4b8aa307244ae
Andre Oosthuizen <andre@ittechsystems.co.za>
2019-07-29 12:50:48 +0200

    update 2

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "images")));
-app.use("/", express.static(path.join(__dirname, "angular")));
+// app.use("/", express.static(path.join(__dirname, "angular")));
 
 
 // app.use((req, res, next) => {
@@ -43,8 +43,8 @@ app.use("/", express.static(path.join(__dirname, "angular")));
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "angular", "index.html"));
-});
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "angular", "index.html"));
+// });
 
 module.exports = app;
