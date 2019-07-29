3697124396c0da1f8d595383e437a2b6acc2312a
Andre Oosthuizen <andre@ittechsystems.co.za>
2019-07-29 13:45:00 +0200

    update 6

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/", express.static(path.join(__dirname, "angular")));
 
-
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader(
