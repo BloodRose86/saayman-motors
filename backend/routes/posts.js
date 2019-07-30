34a8c80fc80054d0143aad310b12c41e479b782b
Andre Oosthuizen <andre@ittechsystems.co.za>
2019-07-30 16:15:36 +0200

    new Update with youtube advert

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -7,7 +7,7 @@ const extractFile = require("../middleware/file");
 
 const router = express.Router();
 
-router.post("", checkAuth, extractFile, PostController.createPost);
+router.post("/posts", checkAuth, extractFile, PostController.createPost);
 
 router.put("/:id", checkAuth, extractFile, PostController.updatePost);
 
